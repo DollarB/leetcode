12facefeffae247c4752e94cf821febbf06eee2c
yage <yage@fordeal.com>
2021-03-02 01:45:02 +0800

    Invert Binary Tree

---
+++ Invert Binary Tree/InvertTree.java
@@ -0,0 +1,67 @@
+//翻转一棵二叉树。
+//
+// 示例：
+//
+// 输入：
+//
+//      4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9
+//
+// 输出：
+//
+//      4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1
+//
+// 备注:
+//这个问题是受到 Max Howell 的 原问题 启发的 ：
+//
+// 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+
+
+/**
+ * 翻转二叉树
+ *
+ * @Author: DollarB
+ * @Email: byxochitl@gmail.com
+ * @Date: 2021/03/02 01:39
+ */
+public class InvertTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
