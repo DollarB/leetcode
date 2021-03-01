6b8e39b745c6db7ac55c48b40185a7b1b87c519c
yage <yage@fordeal.com>
2021-03-01 23:54:03 +0800

    Bubble Sort

---
+++ Bubble Sort/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.example.interview.util.sort;
+
+/**
+ * 冒泡排序
+ *
+ * @Author: DollarB
+ * @Email: byxochitl@gmail.com
+ * @Date: 2021/01/19 22:02
+ */
+public class BubbleSort {
+
+    public static int[] bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
