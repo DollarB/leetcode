fb9538a9485c2482f87d9ff530ff654b3fe43cf6
yage <yage@fordeal.com>
2021-03-12 01:29:13 +0800

    Palindrome Number

---
+++ Palindrome Number/PalindromeNumber.java
@@ -0,0 +1,71 @@
+//给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+//
+// 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：x = 121
+//输出：true
+//
+//
+// 示例 2：
+//
+//
+//输入：x = -121
+//输出：false
+//解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+//
+//
+// 示例 3：
+//
+//
+//输入：x = 10
+//输出：false
+//解释：从右向左读, 为 01 。因此它不是一个回文数。
+//
+//
+// 示例 4：
+//
+//
+//输入：x = -101
+//输出：false
+//
+//
+//
+//
+// 提示：
+//
+//
+// -231 <= x <= 231 - 1
+
+/**
+ * 回文数
+ *
+ * @Author: DollarB
+ * @Email: byxochitl@gmail.com
+ * @Date: 2021/03/12 01:27
+ */
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int div = 1;
+        while (x / div >= 10) {
+            div *= 10;
+        }
+        while (x > 0) {
+            int left = x / div;
+            int right = x % 10;
+            if (left != right) {
+                return false;
+            }
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
+    }
+}
