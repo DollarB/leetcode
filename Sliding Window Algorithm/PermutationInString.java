09ec022f956b208e51acf62f2b058d755ba5b57f
yage <yage@fordeal.com>
2021-03-16 01:13:24 +0800

    Permutation In String

---
+++ Sliding Window Algorithm/PermutationInString.java
@@ -0,0 +1,88 @@
+//给定两个字符串 s1 和 s2，写一个函数来判断 s2 是否包含 s1 的排列。
+//
+// 换句话说，第一个字符串的排列之一是第二个字符串的 子串 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入: s1 = "ab" s2 = "eidbaooo"
+//输出: True
+//解释: s2 包含 s1 的排列之一 ("ba").
+//
+//
+// 示例 2：
+//
+//
+//输入: s1= "ab" s2 = "eidboaoo"
+//输出: False
+//
+//
+//
+//
+// 提示：
+//
+//
+// 输入的字符串只包含小写字母
+// 两个字符串的长度都在 [1, 10,000] 之间
+//
+// Related Topics 双指针 Sliding Window
+
+/**
+ * 字符串的排列
+ *
+ * @Author: DollarB
+ * @Email: byxochitl@gmail.com
+ * @Date: 2021/03/16 00:08
+ */
+public class PermutationInString {
+    public boolean checkInclusion(String s1, String s2) {
+        char[] pattern = s1.toCharArray();
+        char[] text = s2.toCharArray();
+
+        int pLen = s1.length();
+        int tLen = s2.length();
+
+        int[] pFreq = new int[26];
+        int[] winFreq = new int[26];
+
+        for (int i = 0; i < pLen; i++) {
+            pFreq[pattern[i] - 'a']++;
+        }
+
+        int pCount = 0;
+        for (int i = 0; i < 26; i++) {
+            if (pFreq[i] > 0) {
+                pCount++;
+            }
+        }
+
+        int left = 0;
+        int right = 0;
+        int winCount = 0;
+        while (right < tLen) {
+            if (pFreq[text[right] - 'a'] > 0) {
+                winFreq[text[right] - 'a']++;
+                if (winFreq[text[right] - 'a'] == pFreq[text[right] - 'a']) {
+                    winCount++;
+                }
+            }
+            right++;
+
+            while (pCount == winCount) {
+                if (right - left == pLen) {
+                    return true;
+                }
+                if (pFreq[text[left] - 'a'] > 0) {
+                    winFreq[text[left] - 'a']--;
+                    if (winFreq[text[left] - 'a'] < pFreq[text[left] - 'a']) {
+                        winCount--;
+                    }
+                }
+                left++;
+            }
+        }
+        return false;
+    }
+}
