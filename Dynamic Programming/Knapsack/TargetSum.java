ad17815e9e8ddc3064b5391894b4d1a80deec6c4
yage <yage@fordeal.com>
2021-03-26 00:13:13 +0800

    Target Sum

---
+++ Dynamic Programming/Knapsack/TargetSum.java
@@ -0,0 +1,60 @@
+package Knapsack;
+
+//给定一个非负整数数组，a1, a2, ..., an, 和一个目标数，S。现在你有两个符号 + 和 -。对于数组中的任意一个整数，你都可以从 + 或 -中选
+//择一个符号添加在前面。
+//
+// 返回可以使最终数组和为目标数 S 的所有添加符号的方法数。
+//
+//
+//
+// 示例：
+//
+// 输入：nums: [1, 1, 1, 1, 1], S: 3
+//输出：5
+//解释：
+//
+//-1+1+1+1+1 = 3
+//+1-1+1+1+1 = 3
+//+1+1-1+1+1 = 3
+//+1+1+1-1+1 = 3
+//+1+1+1+1-1 = 3
+//
+//一共有5种方法让最终目标和为3。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 数组非空，且长度不会超过 20 。
+// 初始的数组的和不会超过 1000 。
+// 保证返回的最终结果能被 32 位整数存下。
+//
+// Related Topics 深度优先搜索 动态规划
+
+/**
+ * 目标和【0-1背包问题】
+ *
+ * @Author: DollarB
+ * @Email: byxochitl@gmail.com
+ * @Date: 2021/03/26 00:12
+ */
+public class TargetSum {
+    public int findTargetSumWays(int[] nums, int S) {
+        int[] dp = new int[2001];
+        dp[nums[0] + 1000] = 1;
+        dp[-nums[0] + 1000] += 1;
+        for (int i = 1; i < nums.length; i++) {
+            int[] next = new int[2001];
+            for (int sum = -1000; sum <= 1000; sum++) {
+                if (dp[sum + 1000] > 0) {
+                    next[sum + nums[i] + 1000] += dp[sum + 1000];
+                    next[sum - nums[i] + 1000] += dp[sum + 1000];
+                }
+            }
+            dp = next;
+        }
+        return S > 1000 ? 0 : dp[S + 1000];
+    }
+}
